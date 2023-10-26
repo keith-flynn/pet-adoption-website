165c83434e477c20545a0a9053eeb215ed296414
Keith Flynn <flynn.keith@gmail.com>
2023-10-26 10:46:27 -0400

    Preliminary fetch testing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-'use-strict';
+require("dotenv").config();
+
+("use-strict");
+
+document.getElementById("fetchButton").addEventListener("click", () => {
+  const accessToken = process.env.ACCESS_TOKEN;
+  const apiURL = "https://api.petfinder.com/v2/animals";
+
+  // Header with 0auth Bearer token
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  // Perform fetch request
+  fetch(apiURL, { method: "GET", headers })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        console.error(`Request failed with status ${response.status}`);
+        return null; // handle error
+      }
+    })
+    .then((data) => {
+      if (data) {
+        console.log("API Response:", data);
+      }
+    })
+    .catch((error) => {
+      console.error("Request error:", error);
+    });
+});
