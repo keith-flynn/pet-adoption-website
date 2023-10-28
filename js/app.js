922c954de0e19bf99beda52103c13d71c4e0d168
Keith Flynn <flynn.keith@gmail.com>
2023-10-28 17:17:29 -0400

    Functioning API calls and iteration
    Any mishandling of the header during the call to the petfinder api
    results in an invalidated token which needs regenerated. This has been
    the source of almost every problem to date getting up and running. Now
    calls are successful and the structure is in place to generate divs to
    hold the returned data.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,42 @@
 
 "use-strict";
 
-document.getElementById("fetchButton").addEventListener("click", () => {
-  const accessToken = ACCESS_TOKEN;
-  const apiURL = "https://api.petfinder.com/v2/animals?type=dog";
+const apiURL = "https://api.petfinder.com/v2/animals?type=dog";
+const headersInfo = {
+  Authorization: `Bearer ${ACCESS_TOKEN}`
+};
 
-  // Header with 0auth Bearer token
-  const headersInfo = {
-    Authorization: `Bearer ${ACCESS_TOKEN}`
-  };
+async function fetchData() {
+  const response = await fetch(apiURL, { method: "GET", headers: headersInfo });
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+}
 
-  // Perform fetch request
-  fetch(apiURL, { method: "GET", headers: headersInfo })
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        console.error(`Request failed with status ${response.status}`);
-        return null; // handle error
-      }
-    })
-    .then((data) => {
-      if (data) {
-        console.log("API Response:", data);
-      }
-    })
-    .catch((error) => {
-      console.error("Request error:", error);
-    });
+function displayResults(data) {
+  if (data) {
+      console.log("API Response:", data);
+      
+      const resultsContainer = document.getElementById("results-container");
+      const animals = data.animals;
+
+      animals.forEach((item) => {
+          const itemDiv = document.createElement("div");
+          itemDiv.textContent = item.name; // any property
+
+          resultsContainer.appendChild(itemDiv);
+      });
+  }
+}
+
+document.getElementById("fetchButton").addEventListener("click", async () => {
+  try {
+    const data = await fetchData();
+    displayResults(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
-
