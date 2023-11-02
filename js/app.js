a045fcab6ca5435b22a2f89b08181c04b961cec1
Keith Flynn <flynn.keith@gmail.com>
2023-11-02 15:59:54 -0400

    New app.js, non-functional

---
+++ js/app.js
@@ -0,0 +1,47 @@
+
+"use-strict";
+
+const apiURL = "http://localhost:5000/api/search?id=69484507";
+
+
+async function fetchData() {
+  const response = await fetch(apiURL, { method: "GET"});
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+}
+
+function displayResults(data) {
+  if (data && data.animals) {
+    console.log('API Response:', data);
+
+    const resultsContainer = document.getElementById('results-container');
+
+    data.data.forEach((animal) => {
+      const itemDiv = document.createElement('div');
+
+      const photo = document.createElement('img');
+      photo.src = animal.photos.small;
+      photo.alt = animal.name;
+      itemDiv.appendChild(photo);
+
+      const name = document.createElement('p');
+      name.textContent = animal.name;
+      itemDiv.appendChild(name);
+
+      resultsContainer.appendChild(itemDiv);
+    });
+  }
+}
+
+document.getElementById("fetchButton").addEventListener("click", async () => {
+  try {
+    const data = await fetchData();
+    displayResults(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
