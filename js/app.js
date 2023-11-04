995d9b407c0b79d23b4a2473fd11be721dd37758
Keith Flynn <flynn.keith@gmail.com>
2023-11-03 23:04:10 -0400

    Return all images that exist

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-
 "use-strict";
 
 const apiURL = "http://localhost:5000/api/";
-
+//const apiURL = "http://localhost:5000/api/search?name=boris";
 
 async function fetchData() {
-  const response = await fetch(apiURL, { method: "GET"});
+  const response = await fetch(apiURL, { method: "GET" });
   if (response.status === 200) {
     const data = await response.json();
     return data;
@@ -16,22 +15,35 @@ async function fetchData() {
 
 function displayResults(data) {
   if (data) {
-    console.log('API Response:', data);
+    console.log("API Response:", data);
 
-    const resultsContainer = document.getElementById('results-container');
+    const resultsContainer = document.getElementById("results-container");
 
     data.data.forEach((animal) => {
-      const itemDiv = document.createElement('div');
+      const itemDiv = document.createElement("div");
 
-      const photo = document.createElement('img');
-      photo.src = animal.primary_photo_cropped.small;
+      const photo = document.createElement("img");
+      if (animal.photos && animal.photos.length > 0) {
+        const firstPhoto = animal.photos[0];
+        if (firstPhoto.small) {
+          photo.src = firstPhoto.small;
+        } else {
+          // No 'small' property in the photo, load default image
+          const defaultImageURL = "../images/small-circle-transparent.png";
+          photo.src = defaultImageURL;
+        }
+      } else {
+        // No photos available, load a default image
+        const defaultImageURL = "../images/small-circle-transparent.png";
+        photo.src = defaultImageURL;
+      }
+      
       photo.alt = animal.name;
       itemDiv.appendChild(photo);
 
-      const name = document.createElement('p');
+      const name = document.createElement("p");
       name.textContent = animal.name;
       itemDiv.appendChild(name);
-
       resultsContainer.appendChild(itemDiv);
     });
   }
