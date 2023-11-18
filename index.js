a4dde0ac6c7260507164d28e5e2bc7034caeb379
Keith Flynn <flynn.keith@gmail.com>
2023-11-18 11:40:56 -0500

    Change hosting middleware to Replit
    Spun up an instance on replit to host and run index.js. Change target
    URL in search.js to hit new target. Changing github deployment to only
    host static index.html. Hold on to your butts.

--- index.js
+++ index.js
@@ -83,76 +83,76 @@ router.get('/:id', function (req, res, next) {
   });
 });
 
-router.post('/', function (req, res, next) {
-  petRepo.insert(req.body, function (data) {
-    res.status(201).json({
-      "status": 201,
-      "statusText": "Created",
-      "message": "New Pet Added.",
-      "data": data
-    });
-  }, function (err) {
-      next(err);
-  });
-});
+// router.post('/', function (req, res, next) {
+//   petRepo.insert(req.body, function (data) {
+//     res.status(201).json({
+//       "status": 201,
+//       "statusText": "Created",
+//       "message": "New Pet Added.",
+//       "data": data
+//     });
+//   }, function (err) {
+//       next(err);
+//   });
+// });
 
-router.put('/:id', function (req, res, next) {
-  petRepo.getById(req.params.id, function (data) {
-    if (data) {
-      // Attempt to update the data
-      petRepo.update(req.body, req.params.id, function (data) {
-        res.status(200).json({
-          "status": 200,
-          "statusText": "OK",
-          "message": "Pet '" + req.params.id + "' updated.",
-          "data": data
-        });
-      });
-    }
-    else {
-      res.status(404).json({
-        "status": 404,
-        "statusText": "Not Found",
-        "message": "Pet '" + req.params.id + "' could not be found.",
-        "error": {
-          "code": "NOT_FOUND",
-          "message": "Pet '" + req.params.id + "' could not be found."
-        }
-      });
-    }
-  }, function(err) {
-    next(err);
-  });
-});
+// router.put('/:id', function (req, res, next) {
+//   petRepo.getById(req.params.id, function (data) {
+//     if (data) {
+//       // Attempt to update the data
+//       petRepo.update(req.body, req.params.id, function (data) {
+//         res.status(200).json({
+//           "status": 200,
+//           "statusText": "OK",
+//           "message": "Pet '" + req.params.id + "' updated.",
+//           "data": data
+//         });
+//       });
+//     }
+//     else {
+//       res.status(404).json({
+//         "status": 404,
+//         "statusText": "Not Found",
+//         "message": "Pet '" + req.params.id + "' could not be found.",
+//         "error": {
+//           "code": "NOT_FOUND",
+//           "message": "Pet '" + req.params.id + "' could not be found."
+//         }
+//       });
+//     }
+//   }, function(err) {
+//     next(err);
+//   });
+// });
 
-router.delete('/:id', function (req, res, next) {
-  petRepo.getById(req.params.id, function (data) {
-    if (data) {
-      // Attempt to delete data
-      petRepo.delete(req.params.id, function (data) {
-        res.status(200).json({
-          "status": 200,
-          "statusText": "OK",
-          "message": "Pet '" + req.params.id + "' is deleted.",
-          "data": data
-        })
-      })
-    }
-    else {
-      res.status(404).json({
-        "status": 404,
-        "statusText": "Not Found",
-        "message": "Pet '" + req.params.id + "' could not be found.",
-        "error": {
-          "code": "NOT_FOUND",
-          "message": "Pet '" + req.params.id + "' could not be found."
-        }
-      });
-    }
-  }, function(err) {
-    next(err);
-  });
-});
+// router.delete('/:id', function (req, res, next) {
+//   petRepo.getById(req.params.id, function (data) {
+//     if (data) {
+//       // Attempt to delete data
+//       petRepo.delete(req.params.id, function (data) {
+//         res.status(200).json({
+//           "status": 200,
+//           "statusText": "OK",
+//           "message": "Pet '" + req.params.id + "' is deleted.",
+//           "data": data
+//         })
+//       })
+//     }
+//     else {
+//       res.status(404).json({
+//         "status": 404,
+//         "statusText": "Not Found",
+//         "message": "Pet '" + req.params.id + "' could not be found.",
+//         "error": {
+//           "code": "NOT_FOUND",
+//           "message": "Pet '" + req.params.id + "' could not be found."
+//         }
+//       });
+//     }
+//   }, function(err) {
+//     next(err);
+//   });
+// });
 
 // router prefixes all with /aip/v1
 app.use('/api/', router);
